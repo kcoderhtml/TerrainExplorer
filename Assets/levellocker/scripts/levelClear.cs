9681bf22afe6cc7397120be84872f53d904e0c25
Coderhtml <kieran@klukas.net>
2021-10-23 13:56:24 -0400

    removed some assets to free up space and added new menu

---
+++ Assets/levellocker/scripts/levelClear.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelClear : MonoBehaviour
+{
+    
+
+    
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("LevelCleared"))
+        {
+
+        }
+        else
+        {
+            PlayerPrefs.SetInt("LevelCleared", 0);
+
+            if (PlayerPrefs.HasKey("LevelClearedCount"))
+            {
+
+            }
+            else
+            {
+                PlayerPrefs.SetInt("LevelClearedCount", 0);
+            }
+        }
+    }
+
+    public void LevelCleared()
+    {
+        int previousBuildIndex = PlayerPrefs.GetInt("LevelCleared");
+        int currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        int previousLevelCount = PlayerPrefs.GetInt("LevelClearedCount");
+
+        if (currentBuildIndex > previousBuildIndex)
+        {
+            PlayerPrefs.SetInt("LevelCleared", SceneManager.GetActiveScene().buildIndex);
+            PlayerPrefs.SetInt("LevelClearedCount", previousLevelCount + 1);
+
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/viklingvillage/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+#if UNITY_2017_2_OR_NEWER
+using UnityEngine.UI;
+#endif
+
+namespace UnityStandardAssets.Utility
+{
+    public class SimpleActivatorMenu : MonoBehaviour
+    {
+        // An incredibly simple menu which, when given references
+        // to gameobjects in the scene
+#if UNITY_2017_2_OR_NEWER
+        public Text camSwitchButton;
+#else
+        public GUIText camSwitchButton;
+#endif
+        public GameObject[] objects;
+
+
+        private int m_CurrentActiveObject;
+
+
+        private void OnEnable()
+        {
+            // active object starts from first in array
+            m_CurrentActiveObject = 0;
+            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+        }
+
+
+        public void NextCamera()
+        {
+            int nextactiveobject = m_CurrentActiveObject + 1 >= objects.Length ? 0 : m_CurrentActiveObject + 1;
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetActive(i == nextactiveobject);
+            }
+
+            m_CurrentActiveObject = nextactiveobject;
+            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+        }
+    }
+}
